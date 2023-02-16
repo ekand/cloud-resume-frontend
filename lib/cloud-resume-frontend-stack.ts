f696e8dc13e69a9ce849a1811631ee12a6fc79ee
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-02-16 09:40:46 -0600

    add website

--- lib/cloud-resume-frontend-stack.ts
+++ lib/cloud-resume-frontend-stack.ts
@@ -1,16 +1,55 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as targets from "aws-cdk-lib/aws-route53-targets";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CloudResumeFrontendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const assetsBucket = new s3.Bucket(this, "CloudResumeBackendBucket", {
+      websiteIndexDocument: "index.html",
+    });
 
-    // The code that defines your stack goes here
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3deploy.Source.asset("./website-dist")],
+      destinationBucket: assetsBucket,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CloudResumeFrontendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const certificate = acm.Certificate.fromCertificateArn(
+      this,
+      "Certificate",
+      // found using aws acm list-certificates --region us-east-1
+      "arn:aws:acm:us-east-1:212702451742:certificate/abf2dd8b-3651-4fe1-9452-56ade04917e1"
+    );
+    const cf = new cloudfront.Distribution(this, "myDist", {
+      defaultBehavior: { origin: new origins.S3Origin(assetsBucket) },
+      domainNames: ["erikresume.com"],
+      certificate,
+    });
+
+    const zone = route53.HostedZone.fromHostedZoneAttributes(
+      this,
+      "erikresume-zone",
+      {
+        zoneName: "erikresume.com",
+        hostedZoneId: "Z0110113OR9PYKNKIA91",
+      }
+    );
+
+    new route53.ARecord(this, "CDNARecord", {
+      zone,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(cf)),
+    });
+
+    new route53.AaaaRecord(this, "AliasRecord", {
+      zone,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(cf)),
+    });
   }
 }
